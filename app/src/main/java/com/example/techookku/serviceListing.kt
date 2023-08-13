9832ed56f18acef0294984e19eeaabbcab9cdf89
246474 <pradeepraju92@gmail.com>
2023-08-13 08:37:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/techookku/serviceListing.kt
@@ -0,0 +1,11 @@
+package com.example.techookku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class serviceListing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service_listing2)
+    }
+}
